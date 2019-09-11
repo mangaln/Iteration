a4dc6bb8e81aab799944a32854ddb1a77dc60192
mangaln <54597656+mangaln@users.noreply.github.com>
2019-09-10 20:56:31 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello many years experience do you have in professional programming");
+                int years = int.Parse(Console.ReadLine());
+                for (int i = 1; i <=years; i++)
+                {
+                    Console.WriteLine("You have " + i + " years of experience");
+
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Please enter Integer");
+            }
+        }
+    }
+}
